12fd28b25e5b7b5a0d60c26d9babff29f364e54e
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-09-13 12:57:45 +0100

    Crawler working

--- db/seeds.js
+++ db/seeds.js
@@ -1,8 +1,6 @@
 const rp         = require("request-promise");
 const cheerio    = require("cheerio");
-const Bluebird   = require("bluebird");
 const mongoose   = require("mongoose");
-mongoose.Promise = Bluebird;
 const config     = require("../config/config");
 const Station    = require("../models/station");
 const url        = "https://en.wikipedia.org/wiki/List_of_former_and_unopened_London_Underground_stations";
@@ -13,61 +11,60 @@ mongoose.connect(config.db);
 
 Station.collection.drop();
 
-function getStations(){
-  return rp(url)
-  .then((body, response) => {
-    const $             = cheerio.load(body);
-    const stationScrape = $("tr td a");
+rp(url)
+.then((html, response) => {
+  const $             = cheerio.load(html);
+  const stationScrape = $(".wikitable:first-of-type tr td:first-child a");
+  const locations     = [];
+  stationScrape.each((i, station) =>{
+    let location = {
+      name: $(station).text(),
+      url:  $(station).attr("href")
+    };
+    locations.push(location);
+  });
+  console.log(`I've found ${locations.length} locations`);
 
-    stationScrape.each((i, station) => {
-      let location = {
-        name: $(station).text(),
-        url:  $(station).attr("href")
+  locations.forEach((location, i) => {
+
+    rp(`https://en.wikipedia.org${location.url}`)
+    .then((body, response) => {
+      const $      = cheerio.load(body);
+      let name     = $("#firstHeading");
+      if (!name) return;
+      name = name.text();
+
+      let image  = $("a.image img:first-of-type");
+      if (image) {
+        image = `https:${image.attr("src")}`;
+      }
+
+      const coords = $("#coordinates span.geo-dec");
+      if (coords.length === 0) return;
+
+      const coordsString = coords.text();
+      let coordsArray    = coordsString.split(" ");
+      if (coordsArray.length < 2) return;
+
+      coordsArray        = coordsArray.map(coord => parseFloat(coord));
+
+      let obj = {
+        name:  name,
+        image: image,
+        lat:   coordsArray[0],
+        lng:   coordsArray[1],
       };
 
-      if (location.url && location.name && relativeUrl(location.url) && doesntStartWithHash(location.url) && validateUrl(`${url}${location.url}`)) {
-        let newUrl = `${url}${location.url}`;
-        // console.log(newUrl);
-        return locations.push(newUrl);
-      }
+      console.log("SAVE OBJECT", obj);
+
+      Station.create(obj, (err, station) => {
+        if (err) return console.error(err);
+        return console.log(`${station.name} was saved`);
+      });
     });
-
-    return Bluebird.mapSeries(locations, location => {
-      return rp(location);
-    });
-  })
-  .then(data => {
-    return Bluebird.map(data, (body, i) => {
-      const $ = cheerio.load(body);
-      const images = $("img");
-
-
-      let stationData = {};
-      if (images) {
-        stationData.url   = locations[i];
-        stationData.image = images[0].attribs("style");
-        return Station.create(stationData);
-      } else {
-        return;
-      }
-    });
-  })
-  .then(data => {
-    // console.log("DONE");
-    // console.log(data);
-  })
-  .catch(console.error);
-}
-
-getStations();
-
-function validateUrl(value){
-  return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
-}
-
-function relativeUrl(value){
-  return !/^http:\/\//.test(value);
-}
+  });
+})
+.catch(console.error);
 
 function doesntStartWithHash(value) {
   return value.indexOf("#") === -1;
