8b5de7bfd993ed18d98a29adb8e9f2d7aa91a991
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-09-16 11:22:56 +0100

    added engines to package.jsonedt

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
 
-   db: process.env.MONGOLAB_URI || "mongodb://localhost/wdi-project-2",
+   db: process.env.MONGODB_URI || "mongodb://localhost/wdi-project-2",
   secret: process.env.SECRET || "Six Feet Underground"
 };
