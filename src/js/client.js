0a671a72af45e8c8b0a3f87d38e7253e0f60c2d6
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-09-16 10:49:01 +0100

    everything functional, final style tweaks to come

--- src/js/client.js
+++ src/js/client.js
@@ -5,11 +5,11 @@ const App = App || {};
 // --------
 
 App.addInfoWindowForStation = function(station, marker) {
-    google.maps.event.addListener(marker, 'click', () => {
+  google.maps.event.addListener(marker, 'click', () => {
     if (typeof this.infowindow != "undefined") this.infowindow.close();
     this.infowindow = new google.maps.InfoWindow({
-    content: `<div id="infoWindow"><h2 class="windowName">${station.name}</h2><img class="windowPic" src="${station.image}"><p>${station.description}</p></div>`
-  });
+      content: `<div id="infoWindow"><h2 class="windowName">${station.name}</h2><img class="windowPic" src="${station.image}"><p>${station.description}</p></div>`
+    });
     this.infowindow.open(this.map, marker);
     this.map.setCenter(marker.getPosition());
     this.map.panBy(0,-180);
@@ -17,8 +17,6 @@ App.addInfoWindowForStation = function(station, marker) {
 };
 
 App.createMarkerForStation = function(station) {
-  console.log(station.lat, station.lng);
-
   let latlng = new google.maps.LatLng(station.lat, station.lng);
   let marker = new google.maps.Marker({
     position: latlng,
@@ -29,120 +27,116 @@ App.createMarkerForStation = function(station) {
   this.addInfoWindowForStation(station, marker);
 };
 
-
-
 App.loopThroughStations = (data) => {
-  console.log(data.stations);
   $.each(data.stations, (index, station) => {
-    console.log("wet");
-    // setTimeout(() => {
-      App.createMarkerForStation(station);
-  //  }, index * 500);
+    App.createMarkerForStation(station);
   });
 };
 
 App.getStations = function() {
-  console.log("slag");
   $.get("http://localhost:3000/api/stations").done(this.loopThroughStations);
 };
 
 App.mapSetup = function() {
-  console.log("hi");
   this.$main.empty().html(`<div id="mapCanvas"></div>`);
   let mapCanvas = document.getElementById('mapCanvas');
 
   let mapOptions = {
     zoom: 10,
     center: new google.maps.LatLng(51.516178,-0.018369),
-    styles: [{"elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#113b92"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#0fa3de"}]},{"elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#00853e"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"color":"#ee2e24"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#e7e7e8"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"color":"#FFED00"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"color":"#b06111"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#f386a1"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"saturation":-100}]}]
+    styles: [{"elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#f5f5f2"},{"visibility":"on"}]},{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","stylers":[{"visibility":"off"}]},{"featureType":"poi.school","stylers":[{"visibility":"off"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#ffffff"},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"visibility":"simplified"},{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"color":"#ffffff"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#71c8d4"}]},{"featureType":"landscape","stylers":[{"color":"#e5e8e7"}]},{"featureType":"poi.park","stylers":[{"color":"#8ba129"}]},{"featureType":"road","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.sports_complex","elementType":"geometry","stylers":[{"color":"#c7c7c7"},{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#a0d3d3"}]},{"featureType":"poi.park","stylers":[{"color":"#91b65d"}]},{"featureType":"poi.park","stylers":[{"gamma":1.51}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","stylers":[{"visibility":"simplified"}]},{"featureType":"road"},{"featureType":"road"},{},{"featureType":"road.highway"}]
   };
 
   this.map = new google.maps.Map(mapCanvas, mapOptions);
   this.getStations();
 };
 
-// --------
-//  AUTH
-// --------
+
 
 App.homepage = function() {
   this.$main.html(`
-    <div class="welcomeBox">
+    <div class="homepage">
+      <div class="centered">
+      <div class="logo">
+        <h2 class="one">SIX</h2>
+        <h2 class="two">FEET</h2>
+      </div>
       <h2>Where Tube Stations find their peace</h2>
       <h4>Please login or register to access the map</h4>
-
+      </div>
     </div>
 
     `);
-};
+  };
 
-App.loggedInState = function(){
-  $(".loggedOut").hide();
-  $(".loggedIn").show();
-  this.mapSetup();
-};
+  App.loggedInState = function(){
+    $(".loggedOut").hide();
+    $(".loggedIn").show();
+    $(".form-container").hide();
+    this.mapSetup();
+  };
 
-App.loggedOutState = function(){
-  $(".loggedOut").show();
-  $(".loggedIn").hide();
-  this.homepage();
-};
+  App.loggedOutState = function(){
+    $(".loggedOut").show();
+    $(".loggedIn").hide();
+    $(".form-container").empty().show();
+    this.homepage();
+  };
 
-App.setToken = function(token){
-  return window.localStorage.setItem("token", token);
-};
+  App.setToken = function(token){
+    return window.localStorage.setItem("token", token);
+  };
 
-App.getToken = function(){
-  return window.localStorage.getItem("token");
-};
+  App.getToken = function(){
+    return window.localStorage.getItem("token");
+  };
 
-App.removeToken = function(){
-  return window.localStorage.clear();
-};
+  App.removeToken = function(){
+    return window.localStorage.clear();
+  };
 
-App.setRequestHeader = function(xhr, settings) {
-  return xhr.setRequestHeader("Authorization", `Bearer ${this.getToken()}`);
-};
+  App.setRequestHeader = function(xhr, settings) {
+    return xhr.setRequestHeader("Authorization", `Bearer ${this.getToken()}`);
+  };
 
-App.ajaxRequest = function(url, method, data, callback){
-  return $.ajax({
-    url,
-    method,
-    data,
-    beforeSend: this.setRequestHeader.bind(this)
-  })
-  .done(callback)
-  .fail(data => {
-    console.log(data);
-  });
-};
+  App.ajaxRequest = function(url, method, data, callback){
+    return $.ajax({
+      url,
+      method,
+      data,
+      beforeSend: this.setRequestHeader.bind(this)
+    })
+    .done(callback)
+    .fail(data => {
+      console.log(data);
+    });
+  };
 
-App.handleForm = function(){
-  event.preventDefault();
+  App.handleForm = function(){
+    event.preventDefault();
 
-  let url    = `${App.apiUrl}${$(this).attr("action")}`;
-  let method = $(this).attr("method");
-  let data   = $(this).serialize();
+    let url    = `${App.apiUrl}${$(this).attr("action")}`;
+    let method = $(this).attr("method");
+    let data   = $(this).serialize();
 
-  $(this).trigger("reset");
+    $(this).trigger("reset");
 
-  return App.ajaxRequest(url, method, data, (data) => {
-    if (data.token) App.setToken(data.token);
-    App.loggedInState();
-  });
-};
+    return App.ajaxRequest(url, method, data, (data) => {
+      if (data.token) App.setToken(data.token);
+      App.loggedInState();
+    });
+  };
 
-App.logout = function() {
-  event.preventDefault();
-  this.removeToken();
-  this.loggedOutState();
-};
+  App.logout = function() {
+    event.preventDefault();
+    this.removeToken();
+    this.loggedOutState();
+  };
 
-App.register = function() {
-  if (event) event.preventDefault();
-  this.$form.html(`
-<h2>Register</h2>
-  <form method="post" action="/register">
+  App.register = function() {
+    if (event) event.preventDefault();
+    this.$form.html(`
+      <form class="form-inline" method="post" action="/register">
       <div class="form-group">
       <input class="form-control" type="text" name="user[username]" placeholder="Username">
       </div>
@@ -153,19 +147,17 @@ App.register = function() {
       <input class="form-control" type="password" name="user[password]" placeholder="Password">
       </div>
       <div class="form-group">
-      <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password
-      Confirmation">
+      <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Confirm Password">
       </div>
       <input class="btn btn-primary" type="submit" value="Register">
-    </form>
-    `);
-  };
+      </form>
+      `);
+    };
 
-  App.login = function() {
-    event.preventDefault();
-    this.$form.html(`
-      <h2>Login</h2>
-      <form method="post" action="/login">
+    App.login = function() {
+      event.preventDefault();
+      this.$form.html(`
+        <form class="form-inline" method="post" action="/login">
         <div class="form-group">
         <input class="form-control" type="email" name="email" placeholder="Email">
         </div>
@@ -173,28 +165,30 @@ App.register = function() {
         <input class="form-control" type="password" name="password" placeholder="Password">
         </div>
         <input class="btn btn-primary" type="submit" value="Login">
-      </form>
-      `);
-    };
+        </form>
+        `);
+      };
 
-    App.init = function() {
-      this.apiUrl = "http://localhost:3000/api";
-      this.$form  = $('.form-container');
-      this.$main  = $("main");
+      App.init = function() {
+        this.apiUrl = "http://localhost:3000/api";
+        this.$form  = $('.form-container');
+        this.$main  = $("main");
 
-      this.homepage();
+        this.homepage();
 
-      $(".register").on("click", this.register.bind(this));
-      $(".login").on("click", this.login.bind(this));
-      $(".logout").on("click", this.logout.bind(this));
-      this.$form.on("submit", "form", this.handleForm);
+        $(".register").on("click", this.register.bind(this));
+        $(".login").on("click", this.login.bind(this));
+        $(".logout").on("click", this.logout.bind(this));
+        this.$form.on("submit", "form", this.handleForm);
 
 
-      if (this.getToken()) {
-        this.loggedInState();
-      } else {
-        this.loggedOutState();
-      }
-    };
 
-    $(App.init.bind(App));
+
+        if (this.getToken()) {
+          this.loggedInState();
+        } else {
+          this.loggedOutState();
+        }
+      };
+
+      $(App.init.bind(App));
